09fa2e0f98e49e7fb9125782c3e51752270a9907
poll_abben <Polina.Abuziarova@urfu.me>
2021-09-20 13:06:23 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание_Абузярова_Полина
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Владимир Высоцкий Вот она, вот она…");
+            Console.ReadKey();
+            {
+             Console.ForegroundColor = ConsoleColor.Yellow;
+             Console.WriteLine("Вот она, вот она - Наших душ глубина, В ней два сердца плывут, как одно, - Пора занавесить окно.");
+             Console.ReadKey();
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Владимир Высоцкий Вот она, вот она…");
+                Console.WriteLine("Вот она, вот она - Наших душ глубина, В ней два сердца плывут, как одно, - Пора занавесить окно.");
+
+            }
+            ConsoleKeyInfo Consolekeyinfo = Console.ReadKey();
+        }
+    }
+}
